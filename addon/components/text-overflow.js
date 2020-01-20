c39900349421736c935cc720cb150957a90caf7a
bekzod <bekzod@me.com>
2020-01-20 15:16:17 +0500

    use native event handlers

--- addon/components/text-overflow.js
+++ addon/components/text-overflow.js
@@ -1,6 +1,7 @@
 import Component from '@ember/component';
 import { set } from "@ember/object"
 import layout from '../templates/components/text-overflow';
+import { action } from '@ember/object';
 
 export default class TextOverflow extends Component {
 
@@ -11,13 +12,26 @@ export default class TextOverflow extends Component {
     this.set('classNames', ['ember-text-overflow']);
   }
 
-  mouseEnter() {
-    let { element } = this;
-    let el = element.querySelector('.text-overflow-container');
+  didInsertElement() {
+    super.didInsertElement(...arguments);
+    this.element.addEventListener('mouseenter', this.handleMouseEnter);
+    this.element.addEventListener('mouseleave', this.handleMouseLeave);
+  }
+
+  willDestroyElement() {
+    super.willDestroyElement(...arguments);
+    this.element.removeEventListener('mouseenter', this.handleMouseEnter);
+    this.element.removeEventListener('mouseleave', this.handleMouseLeave);
+  }
+
+  @action
+  handleMouseEnter() {
+    let el = this.element.querySelector('.text-overflow-container');
     set(this, 'isOverlayVisible', el.offsetWidth < el.scrollWidth);
   }
 
-  mouseLeave() {
+  @action
+  handleMouseLeave() {
     set(this, 'isOverlayVisible', false);
   }
 
